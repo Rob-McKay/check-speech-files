fd14233c0e4937a550d2fb2bf4785af2feb9cc30
Rob McKay <rob@4mation.co.uk>
2022-03-01 16:22:15 +0000

    Removed unused dependency

--- check-speech-files/main.swift
+++ check-speech-files/main.swift
@@ -6,6 +6,55 @@
 //
 
 import Foundation
+import Speech
 
-print("Hello, World!")
+var complete = false
 
+func recognizeFile(url:NSURL) {
+    guard let myRecognizer = SFSpeechRecognizer() else {
+        // A recognizer is not supported for the current locale
+        "Recognizer not available".data(using: .utf8)
+            .map(FileHandle.standardError.write)
+        exit(2)
+    }
+    
+    if !myRecognizer.isAvailable {
+        // The recognizer is not available right now
+        "Recognizer not available".data(using: .utf8)
+            .map(FileHandle.standardError.write)
+        exit(2)
+    }
+    
+    let request = SFSpeechURLRecognitionRequest(url: url as URL)
+    
+    
+    myRecognizer.recognitionTask(with: request) { (result, error) in
+        guard let result = result else {
+            // Recognition failed, so check error for details and handle it
+            "Failed to convert speech in file \(error.debugDescription)".data(using: .utf8)
+                .map(FileHandle.standardError.write)
+            exit(2)
+        }
+        
+        // Print the speech that has been recognized so far
+        if result.isFinal {
+            print(result.bestTranscription.formattedString)
+            complete = true
+        }
+    }
+}
+
+if CommandLine.argc < 2 {
+    "Usage: \(CommandLine.arguments[0]) <filename>".data(using: .utf8)
+        .map(FileHandle.standardError.write)
+    exit(1)
+}
+
+let args = CommandLine.arguments
+
+
+recognizeFile(url: NSURL(fileURLWithPath: CommandLine.arguments[1]))
+
+let runLoop = RunLoop.current
+let distantFuture = NSDate.distantFuture as NSDate
+while !complete && runLoop.run(mode: RunLoop.Mode.default, before: distantFuture as Date) {}
